465a5c4b57135e0648eeb1da930089bf78d9a68a
Oren Izmirli <dev@izmirli.org>
2021-11-19 16:51:42 +0200

    L10 Coding Challenge done

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,7 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button class="button" @click="removeFromCart">Remove from cart</button>
       </div>
     </div>
   </div>`,
@@ -56,10 +57,13 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
+      },
+      removeFromCart() {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
       }
   },
   computed: {
--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeFromCart(id) {
+            const lastInstence = this.cart.lastIndexOf(id)
+            if (lastInstence >= 0) {
+                this.cart.splice(lastInstence, 1)
+            } else {
+                console.log(`no id ${id} at array: ${this.cart}`)
+            }
+        }
+    }
 })
